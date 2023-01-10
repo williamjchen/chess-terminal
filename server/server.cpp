08f2b1de42a5cdda635f4967adacb4f50a5d30af
William Chen <williamchenjian@gmail.com>
2023-01-09 23:36:10 -0500

    change server implementation

--- server/server.cpp
+++ server/server.cpp
@@ -1,65 +1,85 @@
 #include <iostream>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
-
-#define PORT 8080
-
-int main(int argc, char const* argv[]) {
-    sockaddr_in;
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = { 0 };
-    char* hello = "Hello from server";
- 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+  
+int main()
+{
+    // Create a socket
+    int listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (listening == -1)
+    {
+        std::cerr << "Can't create a socket! Quitting" << std::endl;
+        return -1;
     }
  
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET,
-                   SO_REUSEADDR | SO_REUSEPORT, &opt,
-                   sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    // Bind the ip address and port to a socket
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(54000);
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
  
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr*)&address,
-             sizeof(address))
-        < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket
-         = accept(server_fd, (struct sockaddr*)&address,
-                  (socklen_t*)&addrlen))
-        < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = read(new_socket, buffer, 1024);
-    printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
+    bind(listening, (sockaddr*)&hint, sizeof(hint));
  
-    // closing the connected socket
-    close(new_socket);
-    // closing the listening socket
-    shutdown(server_fd, SHUT_RDWR);
-    return 0;
+    // Tell Winsock the socket is for listening
+    listen(listening, SOMAXCONN);
+ 
+    // Wait for a connection
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+ 
+    int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+ 
+    char host[NI_MAXHOST];      // Client's remote name
+    char service[NI_MAXSERV];   // Service (i.e. port) the client is connect on
+ 
+    memset(host, 0, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
+    memset(service, 0, NI_MAXSERV);
+ 
+    if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+    {
+        std::cout << host << " connected on port " << service << std::endl;
+    }
+    else
+    {
+        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+        std::cout << host << " connected on port " << ntohs(client.sin_port) << std::endl;
+    }
+ 
+    // Close listening socket
+    close(listening);
+ 
+    // While loop: accept and echo message back to client
+    char buf[4096];
+ 
+    while (true)
+    {
+        memset(buf, 0, 4096);
+ 
+        // Wait for client to send data
+        int bytesReceived = recv(clientSocket, buf, 4096, 0);
+        if (bytesReceived == -1)
+        {
+            std::cerr << "Error in recv(). Quitting" << std::endl;
+            break;
+        }
+ 
+        if (bytesReceived == 0)
+        {
+            std::cout << "Client disconnected " << std::endl;
+            break;
+        }
+ 
+        std::cout << std::string(buf, 0, bytesReceived) << std::endl;
+ 
+        // Echo message back to client
+        send(clientSocket, buf, bytesReceived + 1, 0);
+    }
+ 
+    // Close the socket
+    close(clientSocket);
 }
